fe85cbd1549537a21c27c1a09491fba637e96eda
Jian Guo <guojian0706@gmail.com>
2019-09-30 14:30:42 -0500

    Removed unused functions

--- data_process.py
+++ data_process.py
@@ -33,7 +33,7 @@ def get_description(column_name, schema):
     return desc
 
 # Function to separate strings in a column content
-def split_column_content(df,col1,col2=None,delimiter=';'):
+def split_column_content(df, col1, col2=None, delimiter=';'):
     '''
     INPUT:
         - df : a dataframe of inerest 
@@ -46,7 +46,7 @@ def split_column_content(df,col1,col2=None,delimiter=';'):
     new_df.reset_index(drop=True)
     return new_df
 
-def split_and_concat(df,col1,col2,delimiter=';'):
+def split_and_concat(df, col1, col2, delimiter=';'):
     '''
     INPUT:
         - df : a dataframe of inerest
@@ -58,11 +58,23 @@ def split_and_concat(df,col1,col2,delimiter=';'):
     '''
     new_df = pd.DataFrame(columns = [col1, col2])
     for index, row in df.iterrows():
-        columns = row[col1].new_df(delimeter)
+        columns = row[col1].split(delimiter)
         for col in columns:
             new_df.loc[len(new_df)] = [col, row[col2]]
     return new_df
 
+def group_and_agg(df, col1, col2='ConvertedComp',agg_type='median' ):
+    '''
+    INPUT:
+        - df : a dataframe of inerest
+        - col1 - string : a column you want to group
+        - col2 - string : a column you want to apply aggregation
+        - agg_type - string : how you want col2 to be calculated
+    OUTPUT:
+        - new_df : a new dataframe 
+    '''
+    new_df = df.groupby(col1).agg({col2:agg_type}).reset_index().sort_values(col2,ascending=False)
+    return new_df
 def count_and_plot(s,title):
     '''
     INPUT:
@@ -80,7 +92,7 @@ def count_and_plot(s,title):
     plt.title(title)
     plt.grid(axis='x',linestyle='--')
     plt.xlabel('Ratio')
-    plt.savefig(title)
+    # plt.savefig(title)
 
 def plot_value_counts(df, col):
     '''
@@ -122,108 +134,3 @@ def total_count(df, col1, col2, look_for):
     new_df.columns = [col1, col2]
     new_df.sort_values('count', ascending=False, inplace=True)
     return new_df
-
-
-def clean_data(df):
-    '''
-    INPUT
-    df - pandas dataframe 
-    
-    OUTPUT
-    X - A matrix holding all of the variables you want to consider when predicting the response
-    y - the corresponding response vector
-    
-    This function cleans df using the following steps to produce X and y:
-    1. Drop all the rows with no salaries
-    2. Create X as all the columns that are not the Salary column
-    3. Create y as the Salary column
-    4. Drop the Salary, Respondent, and the ExpectedSalary columns from X
-    5. For each numeric variable in X, fill the column with the mean value of the column.
-    6. Create dummy columns for all the categorical variables in X, drop the original columns
-    '''
-    # Drop rows with missing salary values
-    df = df.dropna(subset=['ConvertedComp'], axis=0)
-    y = df['ConvertedComp']
-    
-    #Drop respondent and expected salary columns
-    df = df.drop(['Respondent', 'ConvertedComp', 'CompTotal'], axis=1)
-    
-    # Fill numeric columns with the mean
-    num_vars = df.select_dtypes(include=['float', 'int']).columns
-    for col in num_vars:
-        df[col].fillna((df[col].mean()), inplace=True)
-        
-    # Dummy the categorical variables
-    cat_vars = df.select_dtypes(include=['object']).copy().columns
-    for var in  cat_vars:
-        # for each cat add dummy var, drop original column
-        df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=True)], axis=1)
-    
-    X = df
-    return X, y
-    
-#Use the function to create X and y
-# X, y = clean_data(df)        
-
-
-def find_optimal_lm_mod(X, y, cutoffs, test_size = .30, random_state=42, plot=True):
-    '''
-    INPUT
-    X - pandas dataframe, X matrix
-    y - pandas dataframe, response variable
-    cutoffs - list of ints, cutoff for number of non-zero values in dummy categorical vars
-    test_size - float between 0 and 1, default 0.3, determines the proportion of data as test data
-    random_state - int, default 42, controls random state for train_test_split
-    plot - boolean, default 0.3, True to plot result
-
-    OUTPUT
-    r2_scores_test - list of floats of r2 scores on the test data
-    r2_scores_train - list of floats of r2 scores on the train data
-    lm_model - model object from sklearn
-    X_train, X_test, y_train, y_test - output from sklearn train test split used for optimal model
-    '''
-    r2_scores_test, r2_scores_train, num_feats, results = [], [], [], dict()
-    for cutoff in cutoffs:
-
-        #reduce X matrix
-        reduce_X = X.iloc[:, np.where((X.sum() > cutoff) == True)[0]]
-        num_feats.append(reduce_X.shape[1])
-
-        #split the data into train and test
-        X_train, X_test, y_train, y_test = train_test_split(reduce_X, y, test_size = test_size, random_state=random_state)
-
-        #fit the model and obtain pred response
-        lm_model = LinearRegression(normalize=True)
-        lm_model.fit(X_train, y_train)
-        y_test_preds = lm_model.predict(X_test)
-        y_train_preds = lm_model.predict(X_train)
-
-        #append the r2 value from the test set
-        r2_scores_test.append(r2_score(y_test, y_test_preds))
-        r2_scores_train.append(r2_score(y_train, y_train_preds))
-        results[str(cutoff)] = r2_score(y_test, y_test_preds)
-
-    if plot:
-        plt.plot(num_feats, r2_scores_test, label="Test", alpha=.5)
-        plt.plot(num_feats, r2_scores_train, label="Train", alpha=.5)
-        plt.xlabel('Number of Features')
-        plt.ylabel('Rsquared')
-        plt.title('Rsquared by Number of Features')
-        plt.legend(loc=1)
-        plt.show()
-
-    best_cutoff = max(results, key=results.get)
-
-    #reduce X matrix
-    reduce_X = X.iloc[:, np.where((X.sum() > int(best_cutoff)) == True)[0]]
-    num_feats.append(reduce_X.shape[1])
-
-    #split the data into train and test
-    X_train, X_test, y_train, y_test = train_test_split(reduce_X, y, test_size = test_size, random_state=random_state)
-
-    #fit the model
-    lm_model = LinearRegression(normalize=True)
-    lm_model.fit(X_train, y_train)
-
-    return r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test
-
